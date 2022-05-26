6f69a9178d1aa7881ed3638bb5e6ee02a285c158
erlowery <106184708+erlowery@users.noreply.github.com>
2022-05-26 13:57:44 -0500

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,20 +1,12 @@
-/**
- * class name: Variables.java
- * author: ________
- * date:
- * version:
- * description: This program will display summary of primitive data types.
- * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
- * direction: 1) read the source material
- *            2) summary of primitive data types: byte, short, int, long, float,
- *            double, boolean, char (2 lines per each type)
- *
- */
-public class Variables
+ public class Warmup
 {
     public static void main(String[] args)
     {
         System.out.println("Summary of The Primitive Data Types");
-    }
-
-}
+        System.out.print("A byte is a signed complement of an 8-bit integer.");
+        System.out.print("Its value is between -128 and 127.");
+        System.out.println("A short is a signed compement of a 16-bit integer.");
+        System.out.print("Its value is between -32,768 and 32,767.");
+        System.out.println("A int is a signed complement of a 32-bit integer.");
+        System.out.print("Its Value is between -2 to the 31st and 2 to the 31st -1");
+        System.out.print("");
